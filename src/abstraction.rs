d05dcf757f27d2268385b7e3468c3d27befa0431
stmtc233 <stmtc233@163.com>
2025-08-15 10:00:44 +0800

    a

--- src/abstraction.rs
+++ src/abstraction.rs
@@ -170,8 +170,7 @@ pub(crate) trait Api {
     fn download_img(&self, img_url: &str) -> Result<Vec<u8>> {
         // 使用不带代理的客户端
         let res = self
-            // .noproxy_client()
-            .client()
+            .noproxy_client()
             .get(img_url)
             .send()
             .map_err(net_work_error)?;
